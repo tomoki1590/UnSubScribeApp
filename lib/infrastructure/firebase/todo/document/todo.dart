066e451de751bb576188a685ab62d1af48e1847a
naokiwakata <rockwakata15@gmail.com>
2023-08-12 01:34:26 +0900

    フォルダをinfrastructure層に移動

--- lib/infrastructure/firebase/todo/document/todo.dart
+++ lib/infrastructure/firebase/todo/document/todo.dart
@@ -1,7 +1,8 @@
 // This file is "main.dart"
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:unsbscribe_app/domain/firebase/timestamp_converter.dart';
+
+import '../../timestamp_converter.dart';
 
 part 'todo.freezed.dart';
 part 'todo.g.dart';
