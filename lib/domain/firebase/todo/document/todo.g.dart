9cf547255529fae5d2f0759d18d9f8e4ec97ac7d
naokiwakata <rockwakata15@gmail.com>
2023-08-12 01:20:16 +0900

    freezedでTodoクラスを作成

---
+++ lib/domain/firebase/todo/document/todo.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Todo _$$_TodoFromJson(Map<String, dynamic> json) => _$_Todo(
+      todoId: json['todoId'] as String? ?? '',
+      description: json['description'] as String? ?? '',
+      createdAt:
+          const TimestampConverter().fromJson(json['createdAt'] as Timestamp?),
+    );
+
+Map<String, dynamic> _$$_TodoToJson(_$_Todo instance) => <String, dynamic>{
+      'todoId': instance.todoId,
+      'description': instance.description,
+      'createdAt': const TimestampConverter().toJson(instance.createdAt),
+    };
