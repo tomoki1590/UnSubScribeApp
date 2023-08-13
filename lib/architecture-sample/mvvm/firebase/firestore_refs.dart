9c29a997b399b206698cae5c6dc3b52fb0d86bd1
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-08-13 21:49:35 +0900

    feat: 比較的にシンプルな MVVM の基盤作成

---
+++ lib/architecture-sample/mvvm/firebase/firestore_refs.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: flutter_style_todos
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../model/todo/todo.dart';
+
+final _db = FirebaseFirestore.instance;
+
+/// todos コレクションの参照。
+final todosRef = _db.collection('todos').withConverter<Todo>(
+      fromFirestore: (ds, _) {
+        return Todo.fromDocumentSnapshot(ds);
+      },
+      toFirestore: (obj, _) => obj.toJson(),
+    );
+
+/// todo ドキュメントの参照。
+DocumentReference<Todo> todoRef({required String todoId}) =>
+    todosRef.doc(todoId);
--- lib/presentation/screen/home_screen.dart
+++ lib/presentation/screen/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:unsbscribe_app/presentation/screen_navigation.dart';
 
 import '../../architecture-sample/cobo-/presentation/page/todo/todo_page.dart';
+import '../../architecture-sample/mvvm/presentation/view/todo_page.dart'
+    as mvvm;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -18,6 +20,12 @@ class HomeScreen extends StatelessWidget {
                 ScreenNavigation.push(context, const TodoPage());
               },
               child: const Text('サンプルTodo/cobo-architecture'),
+            ),
+            TextButton(
+              onPressed: () {
+                ScreenNavigation.push(context, const mvvm.TodoPage());
+              },
+              child: const Text('サンプルTodo/simple-mvvm-architecture'),
             )
           ],
         ),
