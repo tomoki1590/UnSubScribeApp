0426f887143054794cb6bdf49d96ada7f6e39ded
tomato <kaguratomato1@gmail.com>
2023-08-23 22:13:50 +0900

    model作成及び修正

---
+++ lib/architecture-sample/mvvm/model/subscribe/subscribe.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../firebase/timestamp_converter.dart';
+
+part 'subscribe.freezed.dart';
+part 'subscribe.g.dart';
+
+@freezed
+class Subscribe with _$Subscribe {
+  const factory Subscribe({
+    @Default('') String reason,
+    @TimestampConverter() deadDay,
+  }) = _Subscribe;
+  const Subscribe._();
+  factory Subscribe.fromJson(Map<String, dynamic> json) =>
+      _$SubscribeFromJson(json);
+
+  factory Subscribe.fromDocumentSnapshot(DocumentSnapshot ds) {
+    final data = ds.data()! as Map<String, dynamic>;
+    return Subscribe.fromJson(<String, dynamic>{
+      ...data,
+      'subscribeId': ds.id,
+    });
+  }
+  Map<String, dynamic> toJsonForFirestore() {
+    final json = toJson();
+    return json..remove('subscribeId');
+  }
+}
---
+++ lib/architecture-sample/mvvm/model/subscribe/subscribe.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'subscribe.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Subscribe _$$_SubscribeFromJson(Map<String, dynamic> json) => _$_Subscribe(
+      reason: json['reason'] as String? ?? '',
+      deadDay: json['deadDay'],
+    );
+
+Map<String, dynamic> _$$_SubscribeToJson(_$_Subscribe instance) =>
+    <String, dynamic>{
+      'reason': instance.reason,
+      'deadDay': instance.deadDay,
+    };
