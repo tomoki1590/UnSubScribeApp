a9614c7c96fc6e572387e33df6248d274dc3b992
tomato <kaguratomato1@gmail.com>
2023-08-29 09:20:21 +0900

    import error 修正

---
+++ lib/presentation/view/app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:unsbscribe_app/presentation/view/component/page_viewer.dart';
+
+import '../theme.dart';
+import 'component/loading.dart';
+
+class App extends ConsumerWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'サブスク管理',
+      debugShowCheckedModeBanner: false,
+      theme: ref.read(themeProvider),
+      home: const PageViewer(),
+      builder: (context, child) {
+        return Consumer(
+          builder: (context, ref, _) {
+            final isLoading = ref.watch(overlayLoadingProvider);
+            return Stack(
+              children: [
+                child!,
+                // ローディング表示
+                if (isLoading) const OverlayLoading()
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
