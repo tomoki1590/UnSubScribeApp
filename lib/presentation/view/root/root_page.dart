39c3bf4f51459036fe0d290036b29a8722db354e
naokiwakata <rockwakata15@gmail.com>
2023-09-01 22:29:44 +0900

    fix import

--- lib/presentation/view/root/root_page.dart
+++ lib/presentation/view/root/root_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:unsubscribe_app/presentation/view/component/page_viewer.dart';
 import 'package:unsubscribe_app/presentation/view_model/root/root_page_view_model.dart';
 
-import '../../view_model/root/loading_state.dart';
+import '../../view_model/root/login_state.dart';
 import '../component/loading.dart';
 
 /// [RootPage]はユーザーのログイン状態に基づいて異なるウィジェットを表示する最も根元にある画面。
