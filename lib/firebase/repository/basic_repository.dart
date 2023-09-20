aacca9385135a43bd86998a99ff607b49683fdf3
tomato <kaguratomato1@gmail.com>
2023-09-20 15:19:48 +0900

    repositoryの削除とViwModelno

--- lib/firebase/repository/basic_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final basicRepositoryProvider = Provider<BasicRepository>(
-  (_) => BasicRepository(),
-);
-
-class BasicRepository {}
--- lib/presentation/view_model/basic_page_view_model.dart
+++ lib/presentation/view_model/basic_page_view_model.dart
@@ -1,26 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../firebase/repository/basic_repository.dart';
-
 class BasicPageState {
-  BasicPageState({required this.isOpen});
-  final bool isOpen;
+  BasicPageState();
 
-  BasicPageState copyWith({bool? isOpen}) {
-    return BasicPageState(
-      isOpen: isOpen ?? this.isOpen,
-    );
+  BasicPageState copyWith() {
+    return BasicPageState();
   }
 }
 
-final basicPageViewModelProvider =
-    StateNotifierProvider<BasicPageViewModel, BasicPageState>(
-  (ref) => BasicPageViewModel(
-    ref.read(basicRepositoryProvider as ProviderListenable<BasicPageState>),
-  ),
-);
-
 class BasicPageViewModel extends StateNotifier<BasicPageState> {
   BasicPageViewModel(super.state);
-  final bool isOpen = false;
 }
