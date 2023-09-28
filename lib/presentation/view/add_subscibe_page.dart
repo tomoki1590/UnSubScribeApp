9734325e3e1baa10e8a02614d009566fe63d7dc4
tomato <kaguratomato1@gmail.com>
2023-09-28 23:17:04 +0900

    appbar切り替え

--- lib/presentation/view/add_subscibe_page.dart
+++ lib/presentation/view/add_subscibe_page.dart
@@ -1,68 +1,42 @@
 import 'package:flutter/material.dart';
 
+import 'tab/master_subscribe_tab.dart';
+import 'tab/sub_add_subscribe_page.dart';
+
 class AddSubscribePage extends StatelessWidget {
   const AddSubscribePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0x00031524),
-      appBar: AppBar(
-        title: Row(
-          children: [
-            TextButton(
-              child: const Text(
-                '主要サブスク',
-                style: TextStyle(fontSize: 16),
-              ),
-              onPressed: () {},
-            ),
-            const Expanded(child: SizedBox()),
-            TextButton(
-              child: const Text(
-                '他のサブスク',
-                style: TextStyle(fontSize: 16),
-              ),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            InkWell(
-              onTap: () {},
-              child: const SizedBox(
-                height: 100,
-                width: 352,
-                child: Card(
-                  color: Color.fromRGBO(24, 24, 24, 1),
-                  child: Center(
-                    child: Row(
-                      children: [
-                        Image(
-                          image: NetworkImage(
-                            'https://onl.bz/gsdG42w',
-                          ),
-                          width: 60,
-                          height: 40,
-                        ),
-                        Text(
-                          'Flutter大学',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: const Color(0x00031524),
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(78),
+          child: AppBar(
+            bottom: const TabBar(
+              tabs: <Widget>[
+                TextButton(
+                  onPressed: null,
+                  child: Text(
+                    '主要サブスク',
+                    style: TextStyle(fontSize: 16),
                   ),
                 ),
-              ),
+                TextButton(
+                  onPressed: null,
+                  child: Text(
+                    '他のサブスク',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [MasterSubscribeTab(), OtherSubscribeTab()],
         ),
       ),
     );
